e3b99f90569246884b9cd1f2c610d0bc8670d190
slanto <slawek.antos@outlook.com>
2017-01-05 18:48:09 +0100

    Solution with tests provided

--- CyclicRotation/CyclicRotation.Core/Solution.cs
+++ CyclicRotation/CyclicRotation.Core/Solution.cs
@@ -2,5 +2,15 @@
 {
     public class Solution
     {
+        public int[] CyclicRotation(int[] a, int k)
+        {
+            var array = new int[a.Length];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                array[(i + k) % a.Length] = a[i];
+            }
+            return array;
+        }
     }
 }
--- CyclicRotation/CyclicRotation.Tests/CyclicRotationTests.cs
+++ CyclicRotation/CyclicRotation.Tests/CyclicRotationTests.cs
@@ -1,12 +1,22 @@
-﻿using Xunit;
+﻿using CyclicRotation.Core;
+using Xunit;
 
 namespace CyclicRotation.Tests
 {    
     public class CyclicRotationTests
     {
-        [Fact]
-        public void TestMethod1()
+        [Theory]
+        [InlineData(new int[] { 3, 8, 9, 7, 6 }, 1, new int[] { 6, 3, 8, 9, 7 })]
+        [InlineData(new int[] { }, 10, new int[] { })]
+        [InlineData(new int[] { 3, 8, 9, 7, 6 }, 3, new int[] { 9, 7, 6, 3, 8 })]
+        [InlineData(new int[] { 3, 8, -9, 7, 6 }, 100, new int[] { 3, 8, -9, 7, 6 })]
+        public void CyclicRotation_RotatesGivenArrayProperly(int[] input, int k, int[] expected)
         {
+            var sut = new Solution();
+
+            var actual = sut.CyclicRotation(input, k);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
